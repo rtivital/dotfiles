0ad29162b366d1f06023d5b3b2a61e0155d73f7c
Vitaly Rtishchev <rtivital@gmail.com>
2017-06-27 22:08:52 +0300

    Add basic profile.sh for every mac

---
+++ profile.sh
@@ -0,0 +1,20 @@
+alias bash_profile='a ~/.bash_profile'
+
+alias g='git'
+alias gs='git status'
+alias ga='git add .'
+alias gcm='git commit -m'
+alias gp='git push'
+alias gclean='git clean -df && git checkout -- .'
+
+alias npr='npm run'
+alias ns='npm start'
+alias nt='npm test'
+alias ni='npm install'
+alias niy='npm init -y'
+
+alias a='atom'
+alias t='touch'
+port() {
+  lsof -i tcp:$1
+}
